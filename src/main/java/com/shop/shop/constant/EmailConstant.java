97f6ed835eb217f42571de04e0c44d4cd224b460
Krzysztof <37305142+qerbitz@users.noreply.github.com>
2022-06-13 15:31:36 +0200

    Update EmailConstant.java

--- src/main/java/com/shop/shop/constant/EmailConstant.java
+++ src/main/java/com/shop/shop/constant/EmailConstant.java
@@ -2,8 +2,6 @@ package com.shop.shop.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "johnjanrambo21@gmail.com";
-    public static final String PASSWORD = "Drukarka1#";
     public static final String FROM_EMAIL = "support_shop@gmail.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Shop App - New Password";
